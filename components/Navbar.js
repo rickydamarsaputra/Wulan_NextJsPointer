9517f872f6ba69389a011f477b1d770903875b9d
rickydamarsaputra <retrocode.rc@gmail.com>
2020-12-11 09:34:41 +0700

    first commit

---
+++ components/Navbar.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Navbar() {
+	return (
+		<header className="py-5">
+			<div className="w-4/5 mx-auto flex items-center justify-between">
+				<img src="/assets/img/d3_logo.svg" alt="d3 undika logo" className="w-16" />
+				<div className="flex space-x-5">
+					<Link href="/">
+						<a className="uppercase font-semibold text-custom">home</a>
+					</Link>
+					<Link href="/posts">
+						<a className="uppercase font-semibold text-custom">posts</a>
+					</Link>
+				</div>
+			</div>
+		</header>
+	);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,71 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import axios from "axios";
+import { FaCaretRight } from "react-icons/fa";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default function index({ posts }) {
+	console.log(posts);
+	return (
+		<div className="w-4/5 mx-auto mt-24">
+			<Head>
+				<title>Landing Page</title>
+			</Head>
+			<div className="grid items-center lg:grid-cols-2 gap-3 lg:gap-0">
+				<div>
+					<div className="text-2xl lg:text-5xl text-custom font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+					<div className="text-sm my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad beatae ut doloremque nisi fuga delectus cum quod! Fugiat, animi cupiditate!</div>
+					<button className="flex items-center text-white bg-custom transition-all duration-300 hover:bg-transparent hover:text-custom border-2 border-custom font-medium focus:outline-none rounded-sm capitalize py-1 px-4">
+						see more
+						<FaCaretRight className="text-xl" />
+					</button>
+				</div>
+				<div className="order-first lg:order-none">
+					<img src="/assets/img/team_work.svg" alt="team work ilustration" />
+				</div>
+			</div>
+			<div className="grid items-center lg:grid-cols-3 gap-10 py-32">
+				<div className="flex items-center justify-center bg-gray-100 py-32">
+					<img src="/assets/img/d3_logo.svg" alt="undika d3 logo" className="w-32" />
+				</div>
+				<div className="lg:col-span-2">
+					<div className="text-2xl">Lorem, ipsum.</div>
+					<div className="text-custom text-2xl mb-3">Lorem, ipsum dolor.</div>
+					<div className="text-sm font-medium">
+						"Lorem ipsum dolor sit amet, consectetur adipisicing elit. At aut officiis rerum. Odit saepe eius consequatur provident, harum quos pariatur reprehenderit magni fuga nihil dolor voluptatum nobis assumenda veritatis, at quam quisquam impedit adipisci iste aliquid beatae veniam accusamus
+						aspernatur. Voluptatibus, explicabo numquam tenetur quos hic nostrum provident veritatis doloremque."
+					</div>
+				</div>
+			</div>
+			<div className="mb-10">
+				<div className="text-custom text-xl capitalize mb-4">latest post</div>
+				<div className="grid lg:grid-cols-3 gap-5">
+					{posts.map((post) => (
+						<div key={post.id}>
+							<img src={post.thumbnail.url} alt={post.title} className="mb-2 h-40 w-full shadow-lg rounded-sm object-cover cursor-pointer transition-all duration-300 transform hover:-translate-y-2" />
+							<Link href="/post/[id]" as={`/post/${post.id}`}>
+								<a className="text-sm font-medium capitalize transition-all duration-300 text-gray-500 hover:text-gray-800">{post.title}</a>
+							</Link>
+						</div>
+					))}
+				</div>
+				<div className="mt-4 flex justify-end">
+					<button className="flex items-center text-white bg-custom transition-all duration-300 hover:bg-transparent hover:text-custom border-2 border-custom font-medium focus:outline-none rounded-sm capitalize py-1 px-4">
+						more post
+						<FaCaretRight className="text-xl" />
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+}
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+export async function getStaticProps(context) {
+	const { API_URL } = process.env;
+	const { data } = await axios.get(`${API_URL}/posts`);
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+	return {
+		props: {
+			posts: data,
+		},
+	};
 }
