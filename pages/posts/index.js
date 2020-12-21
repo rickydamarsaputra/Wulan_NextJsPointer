230c472f2e859d27a05d87d8ae0c022101850bc9
rickydamarsaputra <retrocode.rc@gmail.com>
2020-12-21 21:21:36 +0700

    adding pagination

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -3,8 +3,22 @@ import moment from "moment";
 import "moment/locale/id";
 import Link from "next/link";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
-export default function posts({ posts }) {
+export default function posts({ posts, page, count }) {
+	const router = useRouter();
+	const lastPage = Math.ceil(count / 6);
+
+	const handlePrev = () => {
+		router.push(`/posts?page=${Number(page) - 1}`);
+		console.log("prev");
+	};
+
+	const handleNext = () => {
+		router.push(`/posts?page=${Number(page) + 1}`);
+		console.log("next");
+	};
+
 	return (
 		<div className="w-4/5 mx-auto py-20">
 			<Head>
@@ -23,18 +37,30 @@ export default function posts({ posts }) {
 					</div>
 				))}
 			</div>
+			<div className="flex justify-between mt-8">
+				<button onClick={handlePrev} disabled={page <= 1} className="flex items-center text-white bg-custom transition-all duration-300 hover:bg-transparent hover:text-custom border-2 border-custom font-medium focus:outline-none rounded-sm capitalize py-1 px-4">
+					previous
+				</button>
+				<button onClick={handleNext} disabled={page >= lastPage} className="flex items-center text-white bg-custom transition-all duration-300 hover:bg-transparent hover:text-custom border-2 border-custom font-medium focus:outline-none rounded-sm capitalize py-1 px-4">
+					next
+				</button>
+			</div>
 		</div>
 	);
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps({ query: { page = 1 } }) {
 	const { API_URL } = process.env;
-	const { data } = await axios.get(`${API_URL}/posts?_sort=createdAt:DESC`);
+	const start = page === 1 ? 0 : (page - 1) * 6;
+	const { data } = await axios.get(`${API_URL}/posts?_limit=6&_start=${start}&_sort=createdAt:DESC`);
+	const postCount = await axios.get(`${API_URL}/posts/count`);
+	console.log({ page, start, postCount: postCount.data });
 
 	return {
 		props: {
 			posts: data,
+			page,
+			count: postCount.data,
 		},
-		revalidate: 1,
 	};
 }
